5eb5d2a11039acaeed5486ea5f243f604e9aa7c9
RobertAzarian <az.roberto1998@gmail.com>
2023-04-05 15:44:00 +0400

    Stage 3/8: Palindromic numbers

--- Main.java
+++ Main.java
@@ -1,36 +1,79 @@
 package numbers;
 
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-//        write your code here
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a natural number:");
-        int number = scanner.nextInt();
 
-        if (number > 0) {
-            boolean isEven = number % 2 == 0;
-            boolean isBuzz = (number % 7 == 0) || (number % 10 == 7);
-            boolean isDuck = false;
+        System.out.println("""
+                Welcome to Amazing Numbers!
+                                
+                Supported requests:
+                - enter a natural number to know its properties;
+                - enter 0 to exit.
+                """);
 
-            String strNum = String.valueOf(number);
-            for (int i = 1; i < strNum.length(); i++) {
-                if (strNum.charAt(i) == '0') {
-                    isDuck = true;
-                    break;
-                }
+        while (true) {
+            System.out.print("Enter a request: ");
+            long number = scanner.nextLong();
+            System.out.println();
+
+            if (number > 0) {
+                System.out.printf(Locale.US, "Properties of %,d\n", number);
+
+                    HashMap<String, Boolean> map = getMap(number);
+
+                    for (String key : map.keySet()) {
+                        System.out.println("\t\t" + key + ": " + map.get(key));
+                    }
+                    System.out.println();
+            } else if (number == 0) {
+                System.out.print("Goodbye!");
+                break;
+            } else {
+                System.out.println("The first parameter should be a natural number or zero.\n");
             }
-
-            System.out.printf("""
-                    Properties of %d
-                            even: %b
-                             odd: %b
-                            buzz: %b
-                            duck: %b
-                    """, number, isEven, !isEven, isBuzz, isDuck);
-        } else {
-            System.out.println("This number is not natural!");
         }
+    }
+
+    public static HashMap<String, Boolean> getMap(long number) {
+        HashMap<String, Boolean> map = new HashMap<>();
+
+        boolean isEven = number % 2 == 0;
+        boolean isOdd = !isEven;
+        boolean isBuzz = (number % 7 == 0) || (number % 10 == 7);
+        boolean isDuck = false;
+        boolean isPalindromic = true;
+
+        String strNum = String.valueOf(number);
+
+        // isDuck
+        for (int i = 1; i < strNum.length(); i++) {
+            if (strNum.charAt(i) == '0') {
+                isDuck = true;
+                break;
+            }
+        }
+
+        // isPalindromic
+        int count = strNum.length();
+        for (int i = 0, j = count - 1; i < (count / 2) || j > (count / 2) - 1; i++, j--) { // 123456789    1234.5678
+            if (strNum.charAt(i) != strNum.charAt(j)) {
+                isPalindromic = false;
+                break;
+            }
+        }
+
+        map.put("even", isEven);
+        map.put("odd", isOdd);
+        map.put("buzz", isBuzz);
+        map.put("duck", isDuck);
+        map.put("palindromic", isPalindromic);
+                return map;
     }
 }
