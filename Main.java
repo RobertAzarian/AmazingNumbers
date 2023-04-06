825e9a7badc1aeac78cbf4d86715e81fc1fd190e
RobertAzarian <az.roberto1998@gmail.com>
2023-04-06 19:46:18 +0400

    Stage 4/8: Gapful numbers

--- Main.java
+++ Main.java
@@ -9,37 +9,105 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        long number;
+        int count;
+        String supRequests = """
+                    Supported requests:
+                    - enter a natural number to know its properties;
+                    - enter two natural numbers to obtain the properties of the list:
+                      * the first parameter represents a starting number;
+                      * the second parameter shows how many consecutive numbers are to be processed;
+                    - separate the parameters with one space;
+                    - enter 0 to exit.
+                    """;
 
-        System.out.println("""
-                Welcome to Amazing Numbers!
-                                
-                Supported requests:
-                - enter a natural number to know its properties;
-                - enter 0 to exit.
-                """);
+        System.out.println("Welcome to Amazing Numbers!\n");
+        System.out.println(supRequests);
 
-        while (true) {
+        do {
             System.out.print("Enter a request: ");
-            long number = scanner.nextLong();
+            String inStr = scanner.nextLine();     // reading line
             System.out.println();
 
-            if (number > 0) {
-                System.out.printf(Locale.US, "Properties of %,d\n", number);
-
-                    HashMap<String, Boolean> map = getMap(number);
-
-                    for (String key : map.keySet()) {
-                        System.out.println("\t\t" + key + ": " + map.get(key));
-                    }
-                    System.out.println();
-            } else if (number == 0) {
-                System.out.print("Goodbye!");
-                break;
-            } else {
-                System.out.println("The first parameter should be a natural number or zero.\n");
+            if ("".equals(inStr)) {     // if str is empty
+                System.out.println(supRequests);
+                continue;
             }
-        }
+            String[] inStrArr = inStr.split(" ");
+
+
+            if (inStrArr.length == 1) {        // checking Number valid (number > 0)
+                try {
+                    number = Long.parseLong(inStrArr[0]);
+                    if (number < 0) {
+                        throw new NumberFormatException();
+                    }
+                } catch(NumberFormatException e) {
+                    System.out.println("The first parameter should be a natural number or zero.\n");
+                    continue;
+                }
+                if (number == 0) {
+                    System.out.println("Goodbye!");
+                    break;
+                }
+
+                HashMap<String, Boolean> map = getMap(number);      // output properties
+                System.out.printf(Locale.US, "Properties of %,d\n", number);
+                for (String key : map.keySet()) {
+                    System.out.println("\t\t" + key + ": " + map.get(key));
+                }
+                System.out.println();
+
+
+            } else if (inStrArr.length == 2) {     // checking Number and Count valid (number > 0 && count > 0)
+                try {
+                    number = Long.parseLong(inStrArr[0]);
+                    if (number < 0) {
+                        throw new NumberFormatException();
+                    }
+                } catch(NumberFormatException e) {
+                    System.out.println("The first parameter should be a natural number or zero.\n");
+                    continue;
+                }
+                try {
+                    count = Integer.parseInt(inStrArr[1]);
+                    if (count <= 0) {
+                        throw new NumberFormatException();
+                    }
+                } catch(NumberFormatException e) {
+                    System.out.println("The second parameter should be a natural number.\n");
+                    continue;
+                }
+
+                for (int i = 0; i < count; i++) {       // output properties
+                    int countTrue = 0;
+                    HashMap<String, Boolean> map = getMap(number + i);
+                    for (String key : map.keySet()) {
+                        if (map.get(key)) {
+                            countTrue++;
+                        }
+                    }
+                    System.out.print((number + i) + " is ");
+                    if (countTrue == 0) {
+                        System.out.println("does not have true properties");
+                    } else {
+                        for (String key : map.keySet()) {
+                            if (map.get(key)) {
+                                System.out.print(key);
+                                countTrue--;
+                                if (countTrue != 0) {
+                                    System.out.print(", ");
+                                }
+                            }
+                        }
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+            }
+        } while (true);
     }
+
 
     public static HashMap<String, Boolean> getMap(long number) {
         HashMap<String, Boolean> map = new HashMap<>();
@@ -49,6 +117,7 @@ public class Main {
         boolean isBuzz = (number % 7 == 0) || (number % 10 == 7);
         boolean isDuck = false;
         boolean isPalindromic = true;
+        boolean isGapful = false;
 
         String strNum = String.valueOf(number);
 
@@ -69,11 +138,20 @@ public class Main {
             }
         }
 
+        // isGapful
+        if (strNum.length() >= 3) {
+            int firstAndEnd = Integer.parseInt(String.valueOf(strNum.charAt(0)) + strNum.charAt(strNum.length() - 1));
+            if (number % firstAndEnd == 0) {
+                isGapful = true;
+            }
+        }
+
         map.put("even", isEven);
         map.put("odd", isOdd);
         map.put("buzz", isBuzz);
         map.put("duck", isDuck);
         map.put("palindromic", isPalindromic);
-                return map;
+        map.put("gapful", isGapful);
+        return map;
     }
 }
